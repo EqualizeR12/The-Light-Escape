28acdd296ef067a7f76127366df067cc5ff1fdc8
Alex_F <alex.fursal2o16@gmail.com>
2022-11-09 22:12:22 +0200

    first commit

---
+++ src/components/component.js
@@ -0,0 +1,26 @@
+require("../scss/mainPage .scss");
+require("../scss/index.scss");
+
+// const {ItcSimpleSlider} = require("./slider");
+
+function testComponent() {
+    console.log("all works fine");
+
+      //Custom
+// const slider = new ItcSimpleSlider(".itcss", {
+//     loop: false,
+//   });
+
+//   const next = document.querySelector('.itcss__control_next');
+//   const prev = document.querySelector('.itcss__control_prev');
+
+//   prev.addEventListener('click', () => {
+//     console.log('ytughijk;l,efadsmklnjbchjvggahjsd')
+//     slider.prev();
+//   })
+//   next.addEventListener('click', () => {
+//     slider.next();
+//   })
+}
+
+module.exports = testComponent;
