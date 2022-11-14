9e5bc751226f82c79f0de86a552d364fc1296692
Aleksandr <aleksandrignashov42@gmail.com>
2022-11-14 21:02:35 +0200

    redeploy on hosting and fix small bugs

--- src/components/preloader.js
+++ src/components/preloader.js
@@ -5,27 +5,27 @@ document.body.style.overflowY = "hidden";
 document.documentElement.style.overflowY = "hidden";
 
 function preloader() {
-  // let loadBar = document.querySelector(".load-progress");
+  let loadBar = document.querySelector(".load-progress");
 
-  // document.body.onload = function () {
-  //   setTimeout(() => {
-  //     for (let i = 1; i <= 10; i++) {
-  //       loadBar.style.width = `${i + 10}0%`;
-  //       console.log(loadBar.style.width);
-  //     }
-  //   }, 500);
+  document.body.onload = function () {
+    setTimeout(() => {
+      for (let i = 1; i <= 10; i++) {
+        loadBar.style.width = `${i + 10}0%`;
+        console.log(loadBar.style.width);
+      }
+    }, 500);
 
-  //   setTimeout(() => {
-  //     let preloader = document.querySelector(".preloader");
+    setTimeout(() => {
+      let preloader = document.querySelector(".preloader");
 
-  //     if (!preloader.classList.contains("preloader-done")) {
-  //       preloader.classList.add("preloader-done");
-  //     }
+      if (!preloader.classList.contains("preloader-done")) {
+        preloader.classList.add("preloader-done");
+      }
 
-  //     document.body.style.overflowY = "scroll";
-  //     document.documentElement.style.overflowY = "scroll";
-  //   }, 4000);
-  // };
+      document.body.style.overflowY = "scroll";
+      document.documentElement.style.overflowY = "scroll";
+    }, 4000);
+  };
 }
 
 module.exports = preloader;
