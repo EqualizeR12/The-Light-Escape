f530575875b694f1ea3198a86b52ddc8ccae48fb
Aleksandr <aleksandrignashov42@gmail.com>
2022-11-14 13:02:46 +0200

    fix some style in firefly and

--- src/components/firefly.js
+++ src/components/firefly.js
@@ -1,16 +1,16 @@
 "use strict";
 
 function firefly() {
-  let countLighters = 33;
+  let countLighters = 26;
   let lightersContainer = document.querySelector("html");
   let img;
 
-  for (let i = 7; i < countLighters; i++) {
+  for (let i = 1; i < countLighters; i++) {
     img = document.createElement("img");
     img.className = `lighters${i} positionFirefly`;
     img.setAttribute("alt", `lighters${i}`);
-    lightersContainer.appendChild(img);
     img.setAttribute("src", `../../src/assets/lighters/lighter5.png`);
+    lightersContainer.appendChild(img);
   }
 }
 
