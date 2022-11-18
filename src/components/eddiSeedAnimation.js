bef0348e3cb1722cb16aeb7d0cc409f7eef1cd90
Aleksandr <aleksandrignashov42@gmail.com>
2022-11-18 13:13:56 +0200

    fix scroll eddi sid on mobile

--- src/components/eddiSeedAnimation.js
+++ src/components/eddiSeedAnimation.js
@@ -4,20 +4,32 @@ function seedEddiAnimation() {
   let seed = document.querySelector(".descr-info__cid");
   let eddi = document.querySelector(".descr-info__eddi");
 
-  seed.style.marginRight = "700px";
-  seed.style.opacity = "0";
-  seed.style.transition = "1s";
+  let isMobile = window.innerWidth <= 600;
 
-  eddi.style.marginLeft = "700px";
-  eddi.style.opacity = "0";
-  eddi.style.transition = "1s";
+  if (isMobile) {
+    seed.style.marginRight = "0px";
+    seed.style.opacity = "1";
+    // seed.style.transition = "1s";
+
+    eddi.style.marginLeft = "0px";
+    eddi.style.opacity = "1"; return
+    // eddi.style.transition = "1s";
+
+  } else {
+    seed.style.marginRight = "700px";
+    seed.style.opacity = "0";
+    seed.style.transition = "1s";
+    eddi.style.marginLeft = "700px";
+    eddi.style.opacity = "0";
+    eddi.style.transition = "1s";
+  }
 
   window.addEventListener("scroll", () => {
     let positionScroll =
       (window.scrollY / (document.body.clientHeight - window.innerHeight)) *
       100;
 
-    if (positionScroll > 75) {
+    if (positionScroll > 75 && !isMobile) {
       seed.style.marginRight = "0px";
       seed.style.opacity = "1";
 
@@ -33,7 +45,7 @@ function seedEddiAnimation() {
       eddi.style.opacity = "0";
     }
 
-    console.log(positionScroll);
+    console.log(positionScroll, isMobile);
   });
 }
 
